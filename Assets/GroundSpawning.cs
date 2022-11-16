af456cc5c63b3fbc15051a7bc5bdb105cea12206
Owen Richards <or1g20@soton.ac.uk>
2022-11-16 12:31:41 +0000

    end of labs session

--- Assets/GroundSpawning.cs
+++ Assets/GroundSpawning.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class GroundSpawning : MonoBehaviour
 {
     private float timeSince;
-    public float spawnInterval = 1.0f;
+    public float spawnInterval;
+    public float initialDelay; //NEED TO IMPLEMENT
     public Vector2 spawnPosition;
-    public GameObject groundPrefab;
+    public GameObject section1;
+    public GameObject section2;
+    public List<GameObject> prefabs = new List<GameObject>();
+    bool spawnedFirst = false;
     // Start is called before the first frame update
     void Start()
     {
         timeSince = 0.0f;
+        prefabs.Add(section1);
+        prefabs.Add(section2);
+        initialDelay = 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        initialDelay -= Time.deltaTime;
         timeSince += Time.deltaTime;
-        if (timeSince > spawnInterval){
+        if ((timeSince > spawnInterval) || !spawnedFirst && initialDelay < 0.0f){
+            spawnedFirst = true;
             Debug.Log("ground spawned");
-            GameObject justSpawned = Instantiate(groundPrefab);
-            justSpawned.transform.position = spawnPosition;
+            spawnRandom(0);
             timeSince = 0.0f;
         }
+    }
+
+    void spawnRandom (float yOffset){
+            var r = new System.Random();
+            int choice = r.Next(0,prefabs.Count);
+            Vector2 pos = new Vector2(spawnPosition.x, spawnPosition.y + yOffset);
+            GameObject justSpawned = Instantiate(prefabs[choice]);
+            justSpawned.transform.position = pos;
     }
 }
