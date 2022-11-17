8c4897b87d2650423c934d16dfb804838f1f9c31
Owen Richards <or1g20@soton.ac.uk>
2022-11-17 22:07:04 +0000

    added tutorial messages

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -6,7 +6,7 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps; // popups are a list of tutorial instructions
     private int popUpIndex; // index of instructions in the tutorial
-    public PlayerMovement pm;
+    //public PlayerMovement pm;
     private int jumpsLeft;
     // Start is called before the first frame update
     void Start()
@@ -41,6 +41,7 @@ public class TutorialManager : MonoBehaviour
             }
             // For the double jump
         }else if(popUpIndex==1){
+            Debug.Log("incrementing");
             if(collision.gameObject.tag=="lastdoublejump"){
                 popUpIndex++;
                 Debug.Log("more UI instructions here if we want to add something to the tutorial");
